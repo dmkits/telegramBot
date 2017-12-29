48e76408c1e71917c0f19c25a773c63390a3631c
ianagez <ianagez88@gmail.com>
2017-12-29 17:14:00 +0200

    in process

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var TelegramBot = require('node-telegram-bot-api');
 var fs=require('fs');
 var _=require('lodash');
+var schedule = require('node-schedule');
+var clients = require('./clients');      console.log("clients=",clients);
 var TOKEN ='500967915:AAGsOgZTHzh5ny69hMe21RALQTJgACqZBVQ';
+
+
+clients.id="2132132";
+console.log("clients=",clients);
 
 var bot= new TelegramBot(TOKEN,{
     polling:true /*метод, с помощью которого бот будет общаться с телеграм api*/
@@ -17,16 +23,43 @@ var picSrcs={
     [KB.cat]:['cat1.jpg','cat2.jpg','cat3.jpg'],
     [KB.car]:['car1.jpg','car2.jpg','car3.jpg']
 };
-bot.onText(/\/start/, function(msg, resp){ console.log("resp=",resp);
-    greetClient(msg);
-   // bot.sendMessage(msg.chat.id, "Hello", {
-   //     reply_markup:{
-   //         keyboard:[
-   //             [{text:"Number","request_contact":true},{text:"Location","request_location":true} ]
-   //         ]
-   //     }
-   // });
+
+var clientIdArr=[]; console.log("clientIdArr=",clientIdArr);
+
+bot.onText(/\/start/, function(msg, resp) {
+    console.log("resp=", resp);
+    //greetClient(msg);
+    //bot.sendMessage(msg.chat.id, "Hello", {
+    //    reply_markup: {
+    //        keyboard: [
+    //            [{text: "Register", "request_contact": true}]
+    //        ]
+    //    }
+    //});
+    var newClientId = msg.from.id; console.log("newClientId=",newClientId);
+    if (clientIdArr.length == 0) {
+        clientIdArr.push(newClientId);
+        console.log("clientIdArr=",clientIdArr);
+        return;
+    }
+    for (var i=0; i<clientIdArr.length;i++) {
+        if (clientIdArr[i]==newClientId){
+            return;
+        }
+        if(clientIdArr[clientIdArr.length-1]){
+            clientIdArr.push(newClientId);
+        }
+        console.log("clientIdArr=",clientIdArr);
+    }
+
 });
+
+//var j=schedule.scheduleJob('/10 * * * * *', function(){
+//    console.log("Schedule msg");
+//    bot.sendMessage('491124507', "Schedule msg");
+//});
+
+
 
 bot.on('message', function(msg){ console.log("msg 31=",msg);
     switch (msg.text){
@@ -44,6 +77,7 @@ bot.on('message', function(msg){ console.log("msg 31=",msg);
             break;
     }
 });
+
 function sendScreenPicture(chatID) {
     bot.sendMessage(chatID, 'Выберите тип картинки:', {
         reply_markup: {
